973327932d76f99031e8676e20f0b8b65beb0b0d
Fuwn <contact@fuwn.me>
2023-07-25 16:16:49 -0700

    fix(due): secure secret key

--- src/due/__init__.py
+++ src/due/__init__.py
@@ -3,6 +3,7 @@ from due.routes import auth, oauth, index
 from flask_cors import CORS
 from dotenv import load_dotenv
 from due.cache import cache
+import os
 
 load_dotenv()
 
@@ -13,6 +14,6 @@ CORS(app)
 app.register_blueprint(auth.bp, url_prefix="/auth")
 app.register_blueprint(oauth.bp, url_prefix="/oauth")
 app.register_blueprint(index.bp)
-app.secret_key = "due.moe"
+app.secret_key = os.getenv("SECRET_KEY")
 
 cache.init_app(app)
