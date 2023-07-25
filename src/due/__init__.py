512d29f65c69ad9e1ba337820e9d95d0c3bb880e
Fuwn <contact@fuwn.me>
2023-07-25 16:26:14 -0700

    feat(due): generic error handler

--- src/due/__init__.py
+++ src/due/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from due.routes import auth, oauth, index
+from due.html import page
 from flask_cors import CORS
 from dotenv import load_dotenv
 from due.cache import cache
@@ -15,5 +16,16 @@ app.register_blueprint(auth.bp, url_prefix="/auth")
 app.register_blueprint(oauth.bp, url_prefix="/oauth")
 app.register_blueprint(index.bp)
 app.secret_key = os.getenv("SECRET_KEY")
+
+
+@app.errorhandler(Exception)
+def error_handler(e):
+    return page(
+        """<p>You have encountered a critcal application error. This means that one or more media entries on your lists have resolved abnormaly.</p>
+        <p></p>
+        <p>Contact <a href="https://anilist.co/user/fuwn">Fuwn</a> on AniList for help; please, attach the error code below.</p>""",
+        f"<pre>{e}</pre>",
+    )
+
 
 cache.init_app(app)
